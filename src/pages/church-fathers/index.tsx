73f5fa78a180d880fd65872e10b6e5640f74a5be
unknown <mikaelmaaher2017@gmail.com>
2025-10-01 21:00:29 +0300

    all

--- src/pages/church-fathers/index.tsx
+++ src/pages/church-fathers/index.tsx
@@ -74,20 +74,20 @@ function index() {
       
       <Image src={anba_athanasius} width={200}alt='M' className='m-auto w-[130px] lg:w-[180px]   ' />
       <p className='text-center text-[19px] font-bold lg:text-[20px] my-7'>  الاباء الرعاة للكنيسة </p>
-      <div className="container mx-auto px-6 py-6 text-right ">
-        <div dir="rtl" className="grid  md:grid-cols-2 lg:grid-cols-3 gap-8 pb-9 mb-10 ">
+      <div className="container mx-auto px-8 py-6 text-right ">
+        <div dir="rtl" className="grid  md:grid-cols-2 lg:grid-cols-3 gap-5 pb-7 mb-7 ">
           {fathersOfTheChurch.map((father) => (
             <div
               key={father?.name} style={{ borderRight: '3px solid #894242ff', borderTopLeftRadius: '10px', borderBottomLeftRadius: '10px' }}
-              className="mb-4 bg-[#ffffff12] shadow-xl   overflow-hidden p-6 border border-[transparent]  transition-all duration-300 transform hover:scale-105 hover:shadow-xl hover:bg-[transparent]  grid grid-cols-[3fr_1fr] gap-4  "
+              className="mb-4 bg-[#ffffff12] shadow-xl   overflow-hidden p-3 lg:p-4 pr-6 border border-[transparent]  transition-all duration-300 transform hover:scale-103 hover:shadow-xl hover:bg-[transparent]  grid grid-cols-[3fr_1fr] gap-4  "
             >
               <div className=' '>
-              <h3 className=" text-sm lg:text-lg font-semibold text-gray-800 text-right">{father.name}</h3>
-              <p className=" text-sm lg:text-lg text-gray-600 mt-2 text-right">
+              <h3 className=" text-sm   font-semibold text-gray-800 text-right">{father.name}</h3>
+              <p className=" text-sm   text-gray-600 mt-2 text-right">
                 <strong>تمت الرسامة : </strong> {father.birthDate}
               </p>
               {father.deathDate && (
-                <p className=" text-sm lg:text-lg text-gray-600 mt-2 text-right">
+                <p className=" text-sm   text-gray-600 mt-2 text-right">
                   <strong>و صار قمصا بعام :</strong> {father.deathDate}
                 </p>
               )}
