8148e7dec7b7932c1371ba505b1a8ab367dd9f36
unknown <mikaelmaaher2017@gmail.com>
2025-10-02 07:20:14 +0300

    all

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,14 +1,60 @@
 import React from 'react';
 import Image from 'next/image'
-import icon from './icon.gif'
+import pray from './pray.jpg'
 import image from '../../../public/image.jpg'
  export default function About() {
   return (
     <div>
 
-      <p className='text-center text-[20px] lg:text-[22px] font-bold  mt-5 mb-777'>تاريخ الكنيسة</p>
+ 
+      <div dir='rtl' className="grid grid-cols-1 lg:grid-cols-[1fr_3fr] gap-8 mx-auto p-5 lg:p-10 pb-20 text-right ">
+                <Image  style={{
+            filter: 'brightness(0.85) contrast(1.1) sepia(0.15)  ', // Added filters
+          }} src={pray}  alt="About Us" className="w-full h-auto rounded-lg shadow-lg mix-blend-multiply" />
 
 
+              <section className="rounded-xl shadow p-6 space-y-4 border border-dashed border-gray-300 bg-white/30 opacity-77 backdrop-blur-sm">
+          <h1 className="  font-bold  text-[brown] text-[18px]">💬 الملاك ميخائيل شفيع كنيستنا</h1>
+          <p className="   ">
+            الملاك ميخائيل هو قائد جيش السماء. قوي وشجاع، ويدافع عن أولاد الله.
+            في يوم من الأيام، حاول الشيطان أن يتمرد على الله، لكن الملاك ميخائيل وقف وقال:
+            <strong> "من مثل الله؟"</strong> وقاد الملائكة في حرب ضد الشر وانتصر.
+          </p>
+
+
+            <p className="   ">
+         </p>
+          <p>
+            وهو لا يزال حتى اليوم يحمي أولاد الله ويصلي من أجلهم. نستطيع دائمًا أن نطلب شفاعته عندما نخاف أو نحتاج للحماية.
+          </p>
+  
+        <p>
+            يُصوّر الملاك ميخائيل غالبًا وهو يحمل سيفًا أو رمحًا، يرتدي درعًا ذهبيًا، ويقف منتصرًا فوق الشيطان.
+          </p>
+  <ul className="list-disc pr-6 space-y-2">
+            <li><strong>قائد جيش السماء: </strong> حارب الشيطان وطرحه إلى الأرض (رؤيا 12).</li>
+            <li><strong>مدافع عن شعب الله: </strong> ظهر ليشوع، أنقذ الثلاثة فتية، ودافع عن جسد موسى.</li>
+            <li><strong>بشّر بقيامة المسيح: </strong> كان أول من أعلن الخبر العظيم (متى 28).</li>
+            <li><strong>يسند الشهداء: </strong> يقويهم في العذاب، ويحمي الكنيسة دائمًا.</li>
+          </ul>
+      
+
+      
+ 
+       
+
+    
+
+            <blockquote className="bg-yellow-50 border-r-4 border-yellow-400 p-4 rounded shadow-sm">
+            <p className="text-gray-800 font-medium leading-relaxed">
+              "إِلهُ الرَّجَاءِ يُمَلَأُكُمْ كُلَّ سُرُورٍ وَسَلاَمٍ فِي الإِيمَانِ، لِتَزْدَادُوا فِي الرَّجَاءِ بِقُوَّةِ الرُّوحِ الْقُدُسِ."
+            </p>
+            <span className="block mt-2 text-sm text-yellow-700"> الصفحة لا تزال قيد الانشاء</span>
+          </blockquote>
+        </section>
+           </div>
+ 
+    
 
 
       
