d3a7bf19b40e47a8726849e6354d3f03b5a23f90
unknown <mikaelmaaher2017@gmail.com>
2025-10-04 19:06:28 +0300

    all

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -83,7 +83,7 @@ useEffect(() => {
     
 </div>
 
-    <div className="max-w-2xl mx-auto p-4 bg-white shadow-md rounded-lg my-10">
+    <div className="max-w-[90%] mx-auto p-4 bg-white shadow-md rounded-lg my-10">
       <iframe
         title="SoundCloud Player"
         width="100%"
