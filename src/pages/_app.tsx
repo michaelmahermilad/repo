ebb2c290cc9a38aba52f5092eb476ef1649cee43
unknown <mikaelmaaher2017@gmail.com>
2025-10-24 09:01:01 +0300

    all

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,21 +8,18 @@ import Head from "next/head";
 export default function App({ Component, pageProps }: AppProps) {
 
 
-    const defaultTitle = " كنيسة العذراء مريم بالمليحة";
-  const defaultDescription = "⛪ كنيسة العذراء مريم بالمليحة - الصفحة الرسمية";
+    const defaultTitle = "الموقع الالكتروني لكنيسة العذراء القديسة مريم بالمليحة";
   const defaultImage = "https://elmoli7a-saint-mary-official.vercel.app/image.png";
   const siteUrl = "https://elmoli7a-saint-mary-official.vercel.app";
   return   <>    <Head>
         {/* Default title (will be replaced if page sets one) */}
         <title>{defaultTitle}</title>
-        <meta name="description" content={defaultDescription} />
-
+ 
         {/* Open Graph */}
         <meta property="og:type" content={defaultTitle} />
         <meta property="og:site_name" content={defaultTitle} />
         <meta property="og:title" content={defaultTitle} />
-        <meta property="og:description" content={defaultDescription} />
-        <meta property="og:image" content={defaultImage} />
+         <meta property="og:image" content={defaultImage} />
         <meta property="og:url" content={siteUrl} />
 
         
